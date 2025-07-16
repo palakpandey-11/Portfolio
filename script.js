511b3b658bd4a424de382de82d7a221f322b91a9
palak pandey <palakpandey9608@gmail.com>
2025-07-16 09:17:25 +0530

    project added

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const toggle = document.getElementById("toggle-theme");
-  const body = document.body;
+const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
 
-  toggle.addEventListener("change", () => {
-    body.classList.toggle("light-mode");
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
   });
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.remove('active');
+  });
+});
 
+// script.js me add karo
+window.addEventListener('scroll', function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
